ddecf55c40ae4db2235559c5aaaaa3396b0d5005
Eric Anderson <ericanderson333@Erics-Air.home>
2020-08-28 17:00:55 -0700

    Initial commit

---
+++ Get_News.py
@@ -0,0 +1,115 @@
+from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import requests
+import datetime as dt
+import numpy as np
+import pandas_datareader as web
+
+url = 'https://www.nasdaq.com/api/v1/news-headlines-fetcher/{}/{}/30'
+tickers = ['ENTER TICKER HERE']
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+    "Connection": "keep-alive",
+    "Host": "www.nasdaq.com",
+    "Referer": "http://www.nasdaq.com",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"
+}
+
+
+
+#If we have a watchlist, we will need to loop through each stock in 3 of the functions
+def getNews():
+    #will end up putting for loop to traverse through pages
+    #will increment var by 30 to show 30 different items at a time
+    #Case IDEA: break loop once len of items are less than 30...
+    main_df = pd.DataFrame()
+    page = 0
+    list_len = 30
+    # for i in range(0, 100):
+    while list_len == 30:
+        page_src = requests.get(url.format(tickers[0], str(page)), headers=headers, verify=False)
+        soup = BeautifulSoup(page_src.text, 'html.parser')
+
+        #get the columns we will need (date and headlines)
+        headline_list = [headline.getText().replace('\n', '') for headline in
+                            soup.find_all('p', class_='quote-news-headlines__item-title')]
+        date_list = [date.getText() for date in soup.find_all('span', class_='quote-news-headlines__date')]
+        #we need to get length of list, if changes then we are at the end, we can use either list above
+        list_len = len(headline_list)
+        #we need date to be datetime objects, use np.arrays
+        datetime_obj_list = np.zeros(len(date_list), dtype='datetime64[s]')
+        for i, date in enumerate(date_list):
+            if 'hour' in date or 'hours' in date or 'minutes' in date or 'seconds' in date:
+                datetime_obj_list[i] = dt.datetime.date(dt.datetime.now())
+            elif 'day' in date or 'days' in date:
+                datetime_obj_list[i] = from_day(date)
+            #condition for actual dates in string format
+            else:
+                datetime_obj_list[i] = from_month(date)
+
+        for_df = {'Date': datetime_obj_list, 'News': headline_list}
+        temp_df = pd.DataFrame(data=for_df)
+        if main_df.empty:
+            main_df = temp_df
+        else:
+            main_df = main_df.append(temp_df)
+        page += 30
+    main_df = merge_news(main_df)
+    main_df.to_csv('csv_files/{}_news.csv'.format(tickers[0]))
+
+def from_day(date):
+    days = [1,2,3,4,5,6,7]
+    for num in days:
+        if str(num) in date:
+            return str(dt.datetime.date(dt.datetime.now()) - dt.timedelta(days=num))
+
+def from_month(date):
+    months = {'Jan': '1', 'Feb': '2', 'Mar': '3', 'Apr': '4', 'May': '5', 'Jun': '6',
+              'Jul': '7', 'Aug': '8', 'Sep': '9', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+    for month in months:
+        if month in date:
+            date = date.replace(month, months[month]).replace(',', '').replace(' ', '-')
+            return dt.datetime.date(dt.datetime.strptime(date, '%m-%d-%Y'))
+def merge_news(df):
+    to_merge = lambda a: ". ".join(a)
+    df = df.groupby(by='Date', as_index=False).agg({'News': to_merge}).reset_index()
+    return df
+
+#get stock data after news
+def getData():
+    #We need the same range of data as the news
+    news_df = pd.read_csv('csv_files/{}_news.csv'.format(tickers[0]))
+    start = news_df['Date'][0]
+    end = news_df['Date'][len(news_df)-1]
+    df = web.DataReader(tickers[0], 'yahoo', start, end)
+    df['Pct Change'] = ((df['Adj Close'] - df['Open']) / df['Adj Close'] * 100)
+    #we need to add values to end up predicting (+ or - change)
+    df['Sign'] = np.nan
+    for i, percent in enumerate(df['Pct Change']):
+        if percent > 1:
+            df.iloc[i, df.columns.get_loc('Sign')] = 1
+        elif percent < -1:
+            df.iloc[i, df.columns.get_loc('Sign')] = -1
+        else:
+            df.iloc[i, df.columns.get_loc('Sign')] = 0
+    df.to_csv('csv_files/{}_data.csv'.format(tickers[0]))
+
+def mergeData():
+    news_df = pd.read_csv('csv_files/{}_news.csv'.format(tickers[0]))
+    data_df = pd.read_csv('csv_files/{}_data.csv'.format(tickers[0]))
+    merged_df = news_df.merge(data_df, how='inner', on='Date', left_index=True)
+    merged_df.drop(columns=['index', 'Unnamed: 0'], inplace=True)
+    merged_df.to_csv('csv_files/{}_merged.csv'.format(tickers[0]))
+
+
+if __name__ == '__main__':
+    getNews()
+    getData()
+    mergeData()
+# df = pd.read_csv('TSLA_news.csv')
+# df.drop(columns=['index', 'Unnamed: 0'], inplace=True)
+# df.to_csv('TSLA_news.csv')
---
+++ README.md
@@ -0,0 +1,33 @@
+#NASDAQ STOCK HEADLINE NEWS SENTIMENT
+This program uses scraped webpages from 'nasdaq.com' for stock news. This data
+can be used to attach to existing stock historical data and be a feature for predicting stock
+change for new sentiment. Most stock news from free api's only date back so far. This program scrapes
+as far as when the NASDAQ first created news sentiment for the stock, and compiles all news into one column.<br />
+Ex. SPY - dating back to 2016
+
+##Packages
+* Pandas
+* Numpy
+* Datetime
+* BeautifulSoup4
+* os
+* Requests
+##Example Results (SPY)
+ *Index* | *Date* | *News Headline* 
+ ---------|--------------|------------
+ 0 | 2016-06-03 | "Mattress Firm Holding Enters Oversold Territory (MFRM). | 3.25 | 2017
+ 100 | 2016-10-11| "New York Times Enters Oversold Territory (NYT). Axalta Coating Systems is Now Oversold (AXTA). Universal Display Becomes Oversold (OLED). | 4.629 | 2014
+ 1106 | 2020-08-11 | ANALYSIS-'Bull everything' rally leaves investors wondering what to own | -5.26 | 2015
+ * Note that all "News Headlines" are combined together. They are separated by periods (.)
+ 
+##Contact
+ Phone: (971) 708-4444<br />
+ Email: ericsanderson333@gmail.com<br />
+ Linkedin: https://www.linkedin.com/in/ericanderson333 <br />
+ Please contact me and send me any questions/advice! Thanks!
+ 
+
+
+ 
+ 
+ 
